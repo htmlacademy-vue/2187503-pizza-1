d0549a71aab0353ae4d82a1219077f720c379bad
VYChirkov <VYChirkov@sportmaster.ru>
2022-08-19 18:27:58 +0300

    добавлено изменение счетчика ингредиентов

--- src/frontend/src/common/components/ItemCounter.vue
+++ src/frontend/src/common/components/ItemCounter.vue
@@ -3,12 +3,18 @@
     <button
       type="button"
       class="counter__button counter__button--minus"
-      disabled
+      @click="count -= 1"
+      :disabled="count == 0"
     >
       <span class="visually-hidden">Меньше</span>
     </button>
-    <input type="text" name="counter" class="counter__input" value="0" />
-    <button type="button" class="counter__button counter__button--plus">
+    <input type="text" name="counter" class="counter__input" :value="count" />
+    <button
+      type="button"
+      class="counter__button counter__button--plus"
+      @click="count += 1"
+      :disabled="count == 3"
+    >
       <span class="visually-hidden">Больше</span>
     </button>
   </div>
@@ -16,5 +22,15 @@
 <script>
 export default {
   name: "ItemCounter",
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    AddItem() {
+      this.count += 1;
+    },
+  },
 };
 </script>
