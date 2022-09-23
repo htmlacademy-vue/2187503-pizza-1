7fe865d9975ded4b155265e85a5a985eb1bf2777
VYChirkov <VYChirkov@sportmaster.ru>
2022-09-23 11:59:40 +0300

    вынес BuilderDoughSelector

---
+++ src/frontend/src/builder/components/BuilderDoughSelector.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="content__dough">
+    <div class="sheet">
+      <h2 class="title title--small sheet__title">Выберите тесто</h2>
+      <div class="sheet__content dough">
+        <label
+          v-for="dough in pizza.dough"
+          :key="dough.id"
+          class="dough__input"
+          :class="`dough__input--${doughStatuses[dough.id]}`"
+        >
+          <RadioButton
+            name="dought"
+            :value="doughStatuses[dough.id]"
+            :modelValue="value"
+            @change="setDoughCurrent"
+          />
+
+          <b>{{ dough.name }}</b>
+          <span>{{ dough.description }}</span>
+        </label>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import RadioButton from "@/common/components/RadioButton";
+import doughStatuses from "@/common/enums/doughStatuses";
+import pizza from "@/static/pizza.json";
+export default {
+  name: "IndexHome",
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+  components: {
+    RadioButton,
+  },
+  data() {
+    return {
+      pizza,
+      doughStatuses,
+    };
+  },
+
+  methods: {
+    setDoughCurrent(doughCurrent) {
+      this.$emit("input", doughCurrent);
+    },
+  },
+};
+</script>
--- src/frontend/src/views/Index.vue
+++ src/frontend/src/views/Index.vue
@@ -4,28 +4,7 @@
       <div class="content__wrapper">
         <h1 class="title title--big">Конструктор пиццы</h1>
 
-        <div class="content__dough">
-          <div class="sheet">
-            <h2 class="title title--small sheet__title">Выберите тесто</h2>
-            <div class="sheet__content dough">
-              <label
-                v-for="dough in pizza.dough"
-                :key="dough.id"
-                class="dough__input"
-                :class="`dough__input--${doughStatuses[dough.id]}`"
-              >
-                <RadioButton
-                  name="dought"
-                  :value="doughStatuses[dough.id]"
-                  v-model="doughCurrent"
-                />
-
-                <b>{{ dough.name }}</b>
-                <span>{{ dough.description }}</span>
-              </label>
-            </div>
-          </div>
-        </div>
+        <BuilderDoughSelector v-model="doughCurrent" />
 
         <div class="content__diameter">
           <div class="sheet">
@@ -131,24 +110,24 @@
 import misc from "@/static/misc.json";
 import pizza from "@/static/pizza.json";
 import user from "@/static/user.json";
-import doughStatuses from "@/common/enums/doughStatuses";
 import sizeStatuses from "@/common/enums/sizeStatuses";
 import sauceStatuses from "@/common/enums/sauceStatuses";
 import ingredientStatuses from "@/common/enums/ingredientStatuses";
 import RadioButton from "@/common/components/RadioButton";
+import BuilderDoughSelector from "@/builder/components/BuilderDoughSelector";
 import ItemCounter from "../common/components/ItemCounter.vue";
 export default {
   name: "IndexHome",
   components: {
     RadioButton,
     ItemCounter,
+    BuilderDoughSelector,
   },
   data() {
     return {
       misc,
       pizza,
       user,
-      doughStatuses,
       sizeStatuses,
       sauceStatuses,
       ingredientStatuses,
